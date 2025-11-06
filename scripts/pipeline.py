61291945378ff7f089a42caffcd5984fe93df502
LekshmiSuseela <117069322+LekshmiSuseela@users.noreply.github.com>
2025-11-07 00:00:22 +0530

    Update pipeline.py

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -6,9 +6,9 @@ def run_pipeline(data_version='v1'):
     # In a Vertex AI pipeline you'd create components; here we run the scripts sequentially
     env = os.environ.copy()
     if data_version == 'v2':
-        env['DATA_CSV'] = "gs://mlops-474118-artifacts/data/raw/iris_v2.csv"
+        env['DATA_CSV'] = "gs://mlops-474118-artifacts/data/iris_v2.csv"
     else:
-        env['DATA_CSV'] = "gs://mlops-474118-artifacts/data/raw/iris.csv"
+        env['DATA_CSV'] = "gs://mlops-474118-artifacts/data/iris.csv"
     env['MLFLOW_TRACKING_URI'] = os.getenv('MLFLOW_TRACKING_URI', 'http://<MLFLOW_SERVER_IP>:5000')
     subprocess.check_call(['python', 'scripts/train.py'], env=env)
     subprocess.check_call(['python', 'scripts/inference.py'], env=env)
