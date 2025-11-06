da167ef6922fea7e13cf1881de71030b6918d073
LekshmiSuseela <117069322+LekshmiSuseela@users.noreply.github.com>
2025-11-06 21:35:23 +0530

    Add test for data validation function

---
+++ tests/test_data_validation.py
@@ -0,0 +1,7 @@
+from scripts.data_validation import validate_data
+
+def test_validate():
+    try:
+        validate_data('gs://mlops-474118-artifacts/data/raw/iris.csv')
+    except AssertionError as e:
+        assert False, str(e)
