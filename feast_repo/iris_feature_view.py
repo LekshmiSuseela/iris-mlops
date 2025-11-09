3f73921e204553146996ca4a0e2e61b099da2bc1
LekshmiSuseela <117069322+LekshmiSuseela@users.noreply.github.com>
2025-11-09 12:15:39 +0530

    Update iris_feature_view.py

--- feast_repo/iris_feature_view.py
+++ feast_repo/iris_feature_view.py
@@ -14,7 +14,7 @@ df = pd.read_csv('gs://mlops-474118-artifacts/data/iris.csv')
 df["event_timestamp"] = pd.to_datetime(df["event_timestamp"], errors="coerce").dt.tz_localize("UTC")
 
 # Convert to Parquet
-df.to_parquet("data/iris.parquet", index=False)
+df.to_parquet("../data/iris.parquet", index=False)
 
 # Define your data source
 iris_source = FileSource(
